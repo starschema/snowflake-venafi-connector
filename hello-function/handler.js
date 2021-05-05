c09d92a65f635588e7e84fdb925f5e96206f06fe
Fanni Rakosi <rakosif@starschema.net>
2021-05-05 09:30:31 +0200

    [GH-1] Add hello function with serverless deployment file

---
+++ hello-function/handler.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports.hello = async event => {
+  const body = JSON.parse(event.body);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        data: body.data.map((row) => [row[0], 'hello venafi'])
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
